ca8b6cd3b0708d8fcd37c2c0b579c8ceaf51d377
vangandrew <andrw.vang@gmail.com>
2023-05-20 11:30:33 -0700

    Project done

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+    justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+
+        <p className='text-xl mt-20'>
+        Andrew is a data analyst with expertise in SQL, Python,
+        Power BI, HTML, CSS, and JavaScript. What sets Andrew
+        apart is his ability to not only uncover insights that drive
+        real-world impact, but also to present them in a visually
+        compelling and creative way, making complex data more
+        accessible to stakeholders!
+        </p>
+
+        <br />
+
+        <p className='text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati numquam veniam eum adipisci, quos eos delectus, error nesciunt minus, atque vitae. Omnis quae commodi ad reprehenderit quam non rem, iste error aspernatur, corrupti ducimus cumque necessitatibus magnam nesciunt, voluptatibus animi est enim? Adipisci, error rerum? Atque, vero! Sint, ex illum.
+        </p>
+    </div>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import pythonData from "../assets/portfolio/pythonData.png";
+import tableauLogo from "../assets/portfolio/tableauLogo.png";
+import postgreSQL from "../assets/portfolio/postgreSQL.png";
+
+const Portfolio = () => {
+
+    const portfolios = [
+        {
+            id: 1, 
+            src: pythonData,
+            href: "https://github.com/vangandrew/EDA"
+        },
+        {
+            id: 2, 
+            src: tableauLogo,
+            href: "https://public.tableau.com/app/profile/andrew.vang"
+        },
+        {
+            id: 3, 
+            src: postgreSQL,
+            href: "https://github.com/vangandrew/SQL"
+        },
+    ]
+
+  return (
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+                <p className='py-6'>Check out some of my work!</p>
+            </div>
+
+            
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+            {
+                portfolios.map(({id, src, href}) => (
+                    <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                    <a href={href} target="_blank" rel="noopener noreferrer">
+                        <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
+                    </a>
+                    <div className='flex items-center justify-center'>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                    </div>
+                </div>
+                ))
+            }
+              
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
